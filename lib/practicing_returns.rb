088a1fd2cb32a2b984b3770d34b37466167627da
Ignacio Del Busto <gidelbusto@gmail.com>
2020-05-24 20:30:42 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,12 +1,3 @@
-require 'pry'
-
-def hello(array)
-  i = 0
-  while i < array.length
-    yield(array[i])
-    i += 1
-  end
-end
-
+https://learn.co/tracks/full-stack-web-development-v8/module-5-procedural-ruby/section-7-iteration/return-from-yield-statements
 
 hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
